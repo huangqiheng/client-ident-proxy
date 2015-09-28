ad5d732cca17b197e218ea192e7c1b6257547f3f
and huang <huangqiheng@gmail.com>
2015-09-29 02:22:31 +0800

    write empty functions

--- mta_api.php
+++ mta_api.php
@@ -1,16 +1,19 @@
 <?php 
 
 
+//根据个性化配置，获得MTA上下文对象
 function initMTAConfig($configs)
 {
 
 }
 
+//激活MTA，在其他方法调用前调用。并非必须。
 function startStatService($context, $appkeyi, $mtaSdkVersion)
 {
 
 }
 
+//主动触发一次会话。官方的SDK会话统计是自动的，当然这个版本就是手动的了。
 function startNewSession($context)
 {
 

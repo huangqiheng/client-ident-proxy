46bc220127a01500e7dec30d468826ec748e1a57
root <and huang>
2015-09-28 19:27:59 +0800

    add mta sdk api, php version

---
+++ mta_api.php
@@ -0,0 +1,64 @@
+<?php 
+
+
+function initMTAConfig($configs)
+{
+
+}
+
+function startStatService($context, $appkeyi, $mtaSdkVersion)
+{
+
+}
+
+function startNewSession($context)
+{
+
+}
+
+//===============================================
+
+function trackBeginPage($context, $pageName)
+{
+
+}
+
+
+function trackEndPage($context, $pageName)
+{
+
+}
+
+//===============================================
+
+function trackCustomKVEvent($context, $event_id, $properties)
+{
+
+}
+
+function trackCustomBeginKVEvent($context, $event_id, $properties)
+{
+
+}
+
+function trackCustomEndKVEvent($context, $event_id, $properties)
+{
+
+}
+
+//===============================================
+
+function trackCustomEvent($content, $event_id, $args)
+{
+
+}
+
+function trackCustomBeginEvent($content, $event_id, $args)
+{
+
+}
+
+function trackCustomEndEvent($content, $event_id, $args)
+{
+
+}

f498247bfd1afe742797ea110f510497e3fd4587
and huang <huangqiheng@gmail.com>
2015-09-30 00:43:05 +0800

    mta config json comments

--- mta_api.php
+++ mta_api.php
@@ -1,4 +1,42 @@
 <?php 
+/*
+sm	SleepTime minutes
+sv	SDK version
+md	Model of device
+av	App version
+mf	Manufacture
+osv,ov	android sdk version
+ui	IMEI, mobile serial number
+mid	MID, MTA identify
+mc	MAC adress
+aid	android id
+ts	timestamps
+dts	diff time
+ver	version
+ch	channel
+os	operating system type
+ut	user type
+ncts	need check times
+cfg	configure json object
+ia	is the first time activate
+et	event type
+ky	app key
+uid	user id, get from getDeviceID
+
+lg	language
+ram	ram capacity
+rom	rom capacity
+sd	sd card capacity
+tz	timezone
+prod	product name
+dpi	dot per inch, screen resolution 
+sr	screen resolution 
+wf	wifi name now using
+wflist	wifi state list: 
+	bs:	like mac address
+	dBm:	sinal strength
+	ss: 	wifi ssid
+*/
 
 
 //根据个性化配置，获得MTA上下文对象
